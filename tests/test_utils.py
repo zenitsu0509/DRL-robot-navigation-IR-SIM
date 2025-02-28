55c746b5dc3cc01af9b8de72d63412169b207e1d
reinis <ireinisi@gmail.com>
2025-02-28 13:19:35 +0100

    Update test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 from robot_nav.models.SAC.SAC import SAC
 from robot_nav.models.PPO.PPO import PPO, RolloutBuffer
-from robot_nav.utils import get_buffer
+from robot_nav.models.RCPG.RCPG import RCPG
+from robot_nav.utils import get_buffer, RolloutReplayBuffer, ReplayBuffer
 from robot_nav.sim import SIM_ENV
 
 
@@ -27,6 +28,8 @@ def test_buffer():
     )
     assert buffer.count == 0
 
+    assert isinstance(buffer, ReplayBuffer)
+
     prefilled_buffer = get_buffer(
         model=model,
         sim=sim,
@@ -39,6 +42,45 @@ def test_buffer():
         file_names=["test_data.yml"],
     )
     assert prefilled_buffer.count == 100
+
+
+def test_rollout_buffer():
+    model = RCPG(
+        state_dim=185,
+        action_dim=2,
+        max_action=1,
+        device="cpu",
+        save_every=0,
+        load_model=False,
+    )  # instantiate a model
+
+    sim = SIM_ENV
+    buffer = get_buffer(
+        model=model,
+        sim=sim,
+        load_saved_buffer=False,
+        pretrain=False,
+        pretraining_iterations=0,
+        training_iterations=0,
+        batch_size=0,
+        buffer_size=100,
+    )
+    assert buffer.count == 0
+
+    assert isinstance(buffer, RolloutReplayBuffer)
+
+    prefilled_buffer = get_buffer(
+        model=model,
+        sim=sim,
+        load_saved_buffer=True,
+        pretrain=False,
+        pretraining_iterations=0,
+        training_iterations=0,
+        batch_size=0,
+        buffer_size=100,
+        file_names=["test_data.yml"],
+    )
+    assert prefilled_buffer.count == 6
 
 
 def test_ppo_buffer():
